c72636a3d4af8b8b4b4b94060ecd3fab111c6cba
Yutong Li <yli81711@usc.edu>
2022-05-22 18:47:57 -0700

    first version"

--- src/LAMAPFVisualizer.jsx
+++ src/LAMAPFVisualizer.jsx
@@ -165,7 +165,7 @@ class LAMAPFVisualizer extends Component {
       }),
     };
 
-    fetch("http://10.182.0.2:8080/LA-MAPF", req)
+    fetch("http://34.125.119.104:8080/LA-MAPF", req)
       .then((response) => response.json())
       .then((data) => {
         this.setState({
